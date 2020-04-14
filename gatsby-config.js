14e2ac99d70319dea61bfc2782a4f5c82118136b
neilkapadia7 <neilkapadia7@gmail.com>
2020-04-15 01:37:59 +0530

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,27 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#3d5af1`,
+        theme_color: `#3d5af1`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Exim Air Care`,
+        short_name: `Exim Air Care`,
+        start_url: `/`,
+        background_color: `#3d5af1`,
+        theme_color: `#3d5af1`,
+        display: `standalone`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`, `/about/`, `/products/`, `/contact/`, `/404`],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

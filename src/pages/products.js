b731a71d88f07e933fff418bbe9950c6e77585b2
neilkapadia7 <neilkapadia7@gmail.com>
2020-05-11 11:31:46 +0530

    Changes in About

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,13 +1,37 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Mission from "../components/mission"
+import ServiceCard from "../components/ServiceCard"
 
 const products = () => {
   return (
     <Layout>
       <SEO title="Products" />
-      <div className="App">
-        <h1>Products</h1>
+      <div id="sec1-home">
+        <div className="App">
+          <h1 className="title">Our Products</h1>
+          <center>
+            <p className="page-description-para">
+              We are idea-driven, working with a strong focus on design and user
+              experience. Our projects should engage your audience, we want to
+              create wonderful digital things that people love to be part of and
+              use.
+            </p>
+          </center>
+        </div>
+      </div>
+
+      <div>
+        <Mission />
+      </div>
+      <div className="sec3-home">
+        <div className="App">
+          <center>
+            <h1 className="title2">Our Services</h1>
+          </center>
+          <ServiceCard />
+        </div>
       </div>
     </Layout>
   )
